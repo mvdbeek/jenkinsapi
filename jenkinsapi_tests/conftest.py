f2197147aee26672a79860e1a0a3916c4a32447e
Aleksey Maksimov <ctpeko3a@gmail.com>
2016-09-21 21:14:39 +0800

    Removed install of pytest and mock, reconfigured logging

--- jenkinsapi_tests/conftest.py
+++ jenkinsapi_tests/conftest.py
@@ -5,3 +5,8 @@ logging.basicConfig(
     format='%(module)s.%(funcName)s %(levelname)s: %(message)s',
     level=logging.INFO
 )
+
+modules = ['connectionpool']
+for module_name in modules:
+    logger = logging.getLogger(module_name)
+    logger.setLevel(logging.WARNING)
